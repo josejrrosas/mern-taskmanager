a7661b64930f19b0986b0507d28ba94058164047
Jose Rosas <josejrrosas@yahoo.com>
2025-08-24 21:03:59 -0500

    Finished adding all controller handlers for tasks

--- server/src/controllers/tasksController.js
+++ server/src/controllers/tasksController.js
@@ -3,9 +3,9 @@ import Task from "../models/Task.js";
 export const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find().sort({ createdAt: -1 }); //show newest first
-    res.status(200).json(notes);
+    res.status(200).json(tasks);
   } catch (error) {
-    console.error("Error in getAllNotes controller", error);
+    console.error("Error in getAllTasks controller", error);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -23,61 +23,57 @@ export const createTask = async (req, res) => {
   }
 };
 
-// export const getAllNotes = async (req, res) => {
-//   try {
-//     const notes = await Note.find().sort({ createdAt: -1 }); //show newest first
-//     res.status(200).json(notes);
-//   } catch (error) {
-//     console.error("Error in getAllNotes controller", error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
+export const getTaskByID = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    res.json(task);
 
-// export const getNoteByID = async (req, res) => {
-//   try {
-//     const note = await Note.findById(req.params.id);
-//     if (!note) return res.status(404).json({ message: "Note not found" });
-//     res.json(note);
+    res.status(200).json(task);
+  } catch (error) {
+    console.log("Error in getTaskByID controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-//     res.status(200).json(note);
-//   } catch (error) {
-//     console.error("Error in getNoteByID controller", error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
+export const updateTask = async (req, res) => {
+  try {
+    const { title, notes } = req.body;
+    await Task.findByIdAndUpdate(
+      req.params.id,
+      { title, notes },
+      {
+        new: true,
+      }
+    );
+    if (!updateTask) return res.status(404).json({ message: "Task not found" });
 
-// export const createNote = async (req, res) => {
-//   try {
-//     const { title, content } = req.body;
-//     const note = new Note({ title, content });
+    res.status(200).json(updateTask);
+  } catch (error) {
+    console.error("Error in updateTask conroller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
-//     const savedNote = await note.save();
-//     res.status(201).json(savedNote);
-//   } catch (error) {
-//     console.error("Error in createNote controller", error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
+export const deleteTask = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    await Task.findByIdAndDelete(
+      req.params.id,
+      { title, content },
+      {
+        new: true,
+      }
+    );
 
-// export const updateNote = async (req, res) => {
-//   try {
-//     const { title, content } = req.body;
-//     await Note.findByIdAndUpdate(
-//       req.params.id,
-//       { title, content },
-//       {
-//         new: true,
-//       }
-//     );
-
-//     if (!updateNote) return res.status(404).json({ message: "Note not found" });
-
-//     res.status(200).json(updateNote);
-//   } catch (error) {
-//     console.error("Error in updateNote controller", error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
+    if (!deleteTask)
+      return res.status(404).json({ message: "Task not found." });
+    res.status(200).json({ message: "Deleted Successfully!" });
+  } catch (error) {
+    console.error("Error in deleteTask controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 // export const deleteNote = async (req, res) => {
 //   try {
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllTasks, createTask } from "../controllers/tasksController.js";
+import { getAllTasks, createTask, getTaskByID, updateTask, deleteTask } from "../controllers/tasksController.js";
 const router = express.Router();
 
 router.get("/", getAllTasks);
 router.post("/", createTask);
-  
+router.get("/:id", getTaskByID);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 export default router;
